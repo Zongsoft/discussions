9ba7c63ad009ed363e056349c0a63108fc1f8217
Popeye Zhong <zongsoft@qq.com>
2025-09-27 23:49:43 +0800

    添加了 Avalonia 的表现层。 🍧

---
+++ src/ui/avalonia/shared/Views/MainView.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Zongsoft.Discussions.Presentation.Views;
+
+public partial class MainView : UserControl
+{
+	public MainView()
+	{
+		InitializeComponent();
+	}
+}
---
+++ src/ui/avalonia/shared/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace Zongsoft.Discussions.Presentation.Views;
+
+public partial class MainWindow : Window
+{
+	public MainWindow()
+	{
+		InitializeComponent();
+	}
+}
